99bb8073639d2637857c8019c4f68deba75a2e0d
vpdacoder@gmail.com <vaipatel@utexas.edu>
2018-07-20 23:12:05 -0500

    Auth added

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-var express    = require("express");
-    app        = express();
-    bodyParser = require('body-parser');
-    mongoose   = require('mongoose');
-    Campground = require('./models/campground');
-    Comment    = require('./models/comment');
-    seedDB     = require('./seed');
-    // request    = require('request');
+var express                 = require("express");
+    app                     = express();
+    bodyParser              = require('body-parser');
+    mongoose                = require('mongoose');
+    Campground              = require('./models/campground');
+    Comment                 = require('./models/comment');
+    seedDB                  = require('./seed');
+    passport                = require('passport');
+    User                    = require('./models/user')
+    LocalStrategy           = require('passport-local');
+    passportLocalMongoose   = require('passport-local-mongoose');
 
 mongoose.connect("mongodb://localhost/camper");
 
 seedDB();
+
+
+// PASSPORT CONFIG
+app.use(require("express-session")({
+  secret: "Top secret",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// CONFIG METHODS THAT COME WITH PASSPORT-LOCAL-MONGOOSE
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 app.use(express.static("public"));
@@ -17,11 +35,34 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
 
+
+// ________________
+// MIDDLEWARE
+// ________________
+app.use((req,res,next)=>{
+  //whatever is put into res.locals is available inside the templates
+  res.locals.currentUser = req.user;
+  next();
+});
+
+
+let isLoggedIn = (req,res,next) =>{
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/login");
+}
+
+// ==========================
+// MAIN ROUTES
+// ==========================
+
 app.get("/", (req,res)=>{
   res.render('landing');
 });
 
 app.get('/campgrounds', (req,res)=>{
+  console.log(req.user);
   // Get all campgrounds from //
   Campground.find({}, (err, campgrounds)=> {
     if (err) {
@@ -67,7 +108,7 @@ app.get("/campgrounds/:id", function(req,res){
 // COMMENTS ROUTES
 // ==========================
 
-app.post("/campgrounds/:id/comments", (req,res)=>{
+app.post("/campgrounds/:id/comments", isLoggedIn, (req,res)=>{
   Campground.findById(req.params.id, (err, campground)=>{
     if (err) {
       console.log(err);
@@ -86,7 +127,7 @@ app.post("/campgrounds/:id/comments", (req,res)=>{
   });
 });
 
-app.get("/campgrounds/:id/comments/new", (req,res)=>{
+app.get("/campgrounds/:id/comments/new", isLoggedIn, (req,res)=>{
   Campground.findById(req.params.id, (err, campground)=>{
     if (err) {
       console.log(err);
@@ -95,6 +136,65 @@ app.get("/campgrounds/:id/comments/new", (req,res)=>{
     }
   })
 });
+
+
+
+
+
+// +++++++++++++++++++++++
+// AUTH ROUTES
+// +++++++++++++++++++++++
+
+// SHOW SIGN-UP form
+app.get('/register',(req,res)=> {
+  res.render('register');
+});
+
+// SIGN-UP LOGIC
+app.post('/register', (req,res)=>{
+  let newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, (err, user)=>{
+    if (err) {
+      console.log(err);
+      return res.render('register');
+    } else {
+      // can change "local" to "twitter" or "google" to use different strategies
+      // log the user in
+      passport.authenticate("local")(req,res,()=>{
+        res.redirect('/campgrounds');
+      });
+    }
+  });
+});
+
+
+// SHOW LOGIN FORM
+app.get('/login',(req,res)=> {
+  res.render('login');
+});
+
+// LOGIN LOGIC
+app.post('/login',passport.authenticate("local",{
+   successRedirect: "/campgrounds",
+   failureRedirect: "/login"
+ }),(req,res)=>{
+ });
+
+
+ // LOGOUT
+app.get('/logout', (req,res)=>{
+   req.logout();
+   res.redirect('/campgrounds');
+ })
+
+
+
+
+
+
+
+
+
 
 app.listen(3000, (req,res) =>{
   console.log("3000 is the magic port");
