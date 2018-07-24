6b6bc5a382f9530e2307568feba073c7e6c39a32
vpdacoder@gmail.com <vaipatel@utexas.edu>
2018-07-24 13:23:04 -0500

    Update and Delete functionality for comments added

--- routes/comment.js
+++ routes/comment.js
@@ -49,6 +49,37 @@ router.post("/", isLoggedIn, (req,res)=>{
   });
 });
 
+// EDIT COMMENT ROUTE
+router.get("/:comment_id/edit", (req,res) =>{
+  Comment.findById(req.params.comment_id, (err, comment)=>{
+    if(err){
+      res.redirect('/campgrounds');
+    } else {
+      res.render('comments/edit', {campground_id: req.params.id, comment:comment});
+    }
+  });
+});
 
+// UPDATE Comment
+router.put('/:comment_id', (req,res)=>{
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err,comment)=>{
+    if (err) {
+      res.redirect("back");
+    } else {
+      res.redirect('/campgrounds/'+ req.params.id);
+    }
+  });
+});
+
+// DELETE Comment
+router.delete("/:comment_id", (req,res)=>{
+  Comment.findByIdAndRemove(req.params.comment_id, (err)=>{
+    if(err){
+      res.redirect("back");
+    } else {
+      res.redirect("/campgrounds/"+ req.params.id)
+    }
+  });
+});
 
 module.exports = router;
